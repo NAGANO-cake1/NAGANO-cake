6eb900c0ca13fa6ada7fbd7096201d3ca556248f
AiriNarita <119647023+AiriNarita@users.noreply.github.com>
2023-02-19 09:27:58 +0000

    add: admin/new-edit-form

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,31 @@
 class Admin::ProductsController < ApplicationController
+  def new
+    @product = Product.new
+    @genre = Genre.all
+    @btn = "商品登録"
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_products_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+    @product = Product.new
+    @genre = Genre.all
+    @btn = "編集を保存"
+  end
+
+  def update
+  end
 end
