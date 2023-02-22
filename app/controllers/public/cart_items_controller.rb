b346a02bffa80085ee67f362cf42d160b530f7e4
uenoyam <115358927+uenoyam@users.noreply.github.com>
2023-02-22 08:53:34 +0000

    カートアイテムファイルの変更

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -8,31 +8,33 @@ end
 def update
     cart_item = CartItem.find(params[:id])
     cart_item.update(cart_item_params)
-    redirect_to index
+    redirect_to cart_items_path
     
 end
 
 def create
+   # product_id = cart_item_params[:product_id]
+    #customer_id = cart_item_params[:customer_id]
      if CartItem.find_by(product_id: params[:cart_item][:product_id], customer_id: params[:cart_item][:customer_id]).present?
           cart_item = CartItem.find_by(product_id: params[:cart_item][:product_id], customer_id: params[:cart_item][:customer_id])
-          cart_item.quantity += params[:cart_item][:quantity].to_i
-          cart_item.update
+         cart_item.quantity += params[:cart_item][:quantity].to_i
+          cart_item.update_attribute(:quantity,cart_item.quantity)
      else
           cart_item = CartItem.new(cart_item_params)
           cart_item.save
      end
-     redirect_to index
+     redirect_to  cart_items_path
 end
 
 def destroy
     cart_item = CartItem.find(params[:id])
     cart_item.destroy
-    redirect_to index
+    redirect_to  cart_items_path
 end
 
 def all_destroy
     current_customer.cart_items.destroy_all
-    redirect_to index
+    redirect_to  cart_items_path
 end
 
 
