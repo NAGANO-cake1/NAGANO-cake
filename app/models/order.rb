32da328068663862a1b6bf0b94b7fb38e06c1e40
katsuragi484 <84887671+katsuragi484@users.noreply.github.com>
2023-02-22 06:40:44 +0000

    [Fix]注文情報確認画面

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,6 @@ class Order < ApplicationRecord
     enum order_status: { waiting_for_payment: 0, payment_confirmation: 1, now_at_work: 2, shipping_preparation: 3, sent: 4 } 
     belongs_to :customer 
     has_many :order_details, dependent: :destroy
+    
+    
 end
