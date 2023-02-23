42697e0c210af3de10e8a5ca3eae410f2ece7a68
yurika0501 <titizuzu0501@gmail.com>
2023-02-23 11:24:16 +0000

    [Add]権限調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_admin!, if: :admin_url
+    before_action :authenticate_customer!, if: :customer_url
+
+  def admin_url
+     request.fullpath.include?("/admin")
+  end
+
+  def customer_url
+     request.fullpath.include?("/customer")
+  end
 end
