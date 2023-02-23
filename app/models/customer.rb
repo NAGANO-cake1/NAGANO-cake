46e17287ff12f0221eae6d0871f85ae56673fd53
EC2 Default User <ec2-user@ip-172-31-47-250.ap-northeast-1.compute.internal>
2023-02-23 04:14:44 +0000

    [Add]退会機能完成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,17 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+    # with_options presence: true do
+    #   validates :last_name
+    #   validates :first_name
+    #   validates :last_name_kana
+    #   validates :first_name_kana
+    #   validates :email
+    #   validates :postal_code
+    #   validates :address
+    #   validates :telephone_number
+    # end
+
   has_many :orders, dependent: :destroy
   has_many :cart_items, dependent: :destroy
   has_many :delivery_addresses, dependent: :destroy
