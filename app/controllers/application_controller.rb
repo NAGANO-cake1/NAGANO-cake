1ba960f186cd65e4067159bacfb2ce1a3857a4a4
yurika0501 <titizuzu0501@gmail.com>
2023-02-24 07:22:04 +0000

    [Add]権限設定中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_admin!, if: -> { request.path =~ /^\/admin/ }
     before_action :authenticate_customer!, if: -> { request.path =~ /^\/customer/ }
 
-  private
+#   private
 
-  def admin_url
-     request.fullpath.include?("/admin")
-  end
+#   def authenticate_admin!
+#     #  request.fullpath.include?("/admin")
+#   end
 
-  def customer_url
-     request.fullpath.include?("/customer")
-  end
+#   def authenticate_customer!
+#     #  request.fullpath.include?("/customer")
+#   end
 end
