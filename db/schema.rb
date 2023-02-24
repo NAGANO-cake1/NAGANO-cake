d7ad5b454ee6c4bffccc5aebb95d5ecdda8e93b5
AiriNarita <airiswim.kitty@gmail.com>
2023-02-24 09:22:05 +0000

    add: search-bar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_23_012741) do
+ActiveRecord::Schema.define(version: 2023_02_24_085144) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -124,6 +124,11 @@ ActiveRecord::Schema.define(version: 2023_02_23_012741) do
     t.text "description", null: false
     t.integer "tax_excluded_price", null: false
     t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "searches", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
